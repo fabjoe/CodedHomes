f8aced7ff511a8e3dba0ea697f2fb7ff825fca2d
fabrizioc <fabrizioc@silversea.com>
2017-10-02 18:01:13 +0200

    Adding DBContext

---
+++ CodedHomes.Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodedHomes.Models;
+using System.Data.Entity;
+using System.Configuration;
+
+namespace CodedHomes.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Home> Homes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["CodedHomesDEV"].ConnectionString;
+            }
+        }
+
+        public DataContext() : base(nameOrConnectionString:DataContext.ConnectionString)
+        {
+
+        }
+    }
+}
